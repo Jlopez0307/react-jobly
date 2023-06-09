9c8b3fb71571643f9b20db30946ef27ff2a721ba
Jlopez0307 <55158803+Jlopez0307@users.noreply.github.com>
2023-06-09 19:53:35 -0400

    removed useContext from react import

--- react-jobly-frontend/src/routes/Routes.js
+++ react-jobly-frontend/src/routes/Routes.js
@@ -1,5 +1,5 @@
 //React & React Router
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {Route, Routes, Navigate} from 'react-router-dom';
 
 //Components
